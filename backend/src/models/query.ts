7ef5acfca766a905c2763fda17b455d4efeb59ae
deep-madhvani <deep.madhvani@csu.fullerton.edu>
2023-12-05 19:08:28 -0800

    first commit

---
+++ backend/src/models/query.ts
@@ -0,0 +1,19 @@
+export interface IQuery {
+    keyword: string,
+    products: IProduct[]
+}
+
+export interface INQuery {
+    input: {
+        keyword: string
+    },
+    products: IProduct[]
+}
+
+export interface IProduct {
+    title: string,
+    url: string,
+    price: number,
+    image: string,
+    competitor: string
+}
